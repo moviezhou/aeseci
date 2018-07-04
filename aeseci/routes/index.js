3b669f29d1f94c97266693f54ee7178503550048
moviezhou <moviezhou@gmail.com>
2018-07-05 01:22:07 +0800

    subpage added

--- aeseci/routes/index.js
+++ aeseci/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/market', function(req, res, next) {
+  res.render('market', { title: 'Express' });
+});
+
+router.get('/law', function(req, res, next) {
+  res.render('law', { title: 'Express' });
+});
+
+router.get('/contactus', function(req, res, next) {
+  res.render('contactus', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/supervision', function(req, res, next) {
+  res.render('supervision', { title: 'Express' });
+});
+
+router.get('/agreement', function(req, res, next) {
+  res.render('agreement', { title: 'Express' });
+});
+
 module.exports = router;
